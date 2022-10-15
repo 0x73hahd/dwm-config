dcdede4060e14485e999d1b108f39c1fb364a3d3
0x73hahd <0x73hahd@gmail.com>
2022-10-15 14:15:03 +0200

    make header file for layouts

--- source/config.def.h
+++ source/config.def.h
@@ -35,17 +35,3 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ BROWSER,  NULL,       NULL,       1 << 8,       0,           -1 },
 };
-
-/* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-
-static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
-};
-
