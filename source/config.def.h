7dd5f1ab49caab7b8f92ec173c79401f6cf7b2e1
0x73hahd <0x73hahd@gmail.com>
2022-11-04 16:56:37 +0200

    change default terminal to alacritty

--- source/config.def.h
+++ source/config.def.h
@@ -3,7 +3,7 @@
 #include "colors/dracula.h"
 #include "keys.h"
 
-#define TERMINAL	"konsole"              // default terminal
+#define TERMINAL	"alacritty"              // default terminal
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
