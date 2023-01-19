8414addbb9bd6f74b37b370718835727ea1bea6d
shahd <0x73hahd@gmail.com>
2023-01-19 05:52:29 +0200

    Create space.h

---
+++ source/colors/space.h
@@ -0,0 +1,34 @@
+static const char col_gray1[]       = "#0f161e";
+static const char col_gray3[]       = "#fff7d0";
+static const char col_gray4[]       = "#c358ad";
+static const char col_gray2[]       = "#FF609A";
+static const char col_cyan[]        = "#0f161e";
+
+static const char tag1[]            = "#bf8427";
+static const char tag2[]            = "#5865F2";
+static const char tag3[]            = "#FF5500";
+static const char tag4[]            = "#FF609A";
+static const char tag5[]            = "#842291";
+static const char tag6[]            = "#900C3F";
+static const char tag7[]            = "#67AFA5";
+static const char tag8[]            = "#1DB954";
+static const char tag9[]            = "#FFFFFF";
+
+static const char *colors[][3]      = {
+    /*                 fg         bg         border   */
+    [SchemeNorm]   = { col_gray3, col_gray1, col_cyan },
+    [SchemeSel]    = { col_gray4, col_cyan,  col_gray2  },
+    [SchemeTitle]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+static const char *tagsel[][2] = {
+	{   tag1, col_gray1   },
+	{   tag2, col_gray1   },
+	{   tag3, col_gray1   },
+	{   tag4, col_gray1   },
+	{   tag5, col_gray1   },
+	{   tag6, col_gray1   },
+	{   tag7, col_gray1   },
+	{   tag8, col_gray1   },
+	{   tag9, col_gray1   },
+};
